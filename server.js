07e2a6a92022cf4b72379aa52067dbc73ae8f07e
MersoSoftware <mersosoftware@gmail.com>
2022-10-31 23:22:46 -0300

    heroku

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-// const express = require('express');
+const express = require('express');
 
-// const app = express();
+const app = express();
 
-// const PORT = process.env.PORT || 8082;
+const PORT = process.env.PORT || 8082;
 
-// app.use(express.static(__dirname + '/dist/sistema'));
+app.use(express.static(__dirname + '/dist/sistema'));
 
-// app.get('/*', (req, res) => {
-//     res.sendFile(__dirname + '/dist/sistema/index.html');
+app.get('/*', (req, res) => {
+    res.sendFile(__dirname + '/dist/sistema/index.html');
 
-// });
+});
 
-// app.listen(PORT, () => {
-//     console.log("servidor on line porta "+PORT)
-// })
+app.listen(PORT, () => {
+    console.log("servidor on line porta "+PORT)
+})
